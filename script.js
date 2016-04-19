b0acf4fd0434fc70e71ee3335259592cde403f7f
markalderson <m4rk4ld3rs0n@gmail.com>
2016-04-19 13:15:15 +0200

    Features 1 up to 4

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 angular.module('VitaminQuizApp', ['ngMaterial', 'md.data.table']).
   config(['$compileProvider', function ($compileProvider) {
-    $compileProvider.aHrefSanitizationWhitelist(/data:/);
+    $compileProvider.aHrefSanitizationWhitelist(/data:|mailto:/);
   }]).
-  controller('QuizCtrl', function ($scope, $location, $http) {
+  controller('QuizCtrl', function ($scope, $location, $http, $mdDialog) {
     var quiz_file = $location.search()['quiz'];
     $http.get(quiz_file).then(function (result) {
       $scope.quiz = result.data;
@@ -30,6 +30,15 @@ angular.module('VitaminQuizApp', ['ngMaterial', 'md.data.table']).
       $scope.finished = true;
     };
 
+    $scope.intake = function (vitamin) {
+      return $scope.vitamin2intake[vitamin];
+    };
+
+    $scope.percentage = function (vitamin) {
+      var i = $scope.quiz.vitamins.indexOf(vitamin);
+      return $scope.vitamin2intake[vitamin] / $scope.quiz.thresholds[i] * 100;
+    };
+
     $scope.vitaminsEnough = function () {
       return $scope.quiz.vitamins.filter(function (vitamin, i) {
         var intake = $scope.vitamin2intake[vitamin];
@@ -47,7 +56,15 @@ angular.module('VitaminQuizApp', ['ngMaterial', 'md.data.table']).
     };
 
     $scope.exportAsCsv = function () {
-      var result = $scope.quiz.foods.map(function (food) {
+      var result = '';
+      var not_enough = $scope.vitaminsNotEnough();
+      if (not_enough.length > 0) {
+        result += 'Nutrients you don\'t get enough of\n';
+        result += not_enough.join('\n');
+        result += '\n'
+      }
+      result += 'Your answers\n';
+      result += $scope.quiz.foods.map(function (food) {
         var answer_index = $scope.food2answer[food];
         var answer = $scope.quiz.answers[answer_index];
         var multiplier = $scope.quiz.multipliers[answer_index];
@@ -55,5 +72,28 @@ angular.module('VitaminQuizApp', ['ngMaterial', 'md.data.table']).
         return row.join(',');
       }).join('\n');
       return encodeURI(result);
+    };
+
+    $scope.openResultsDialog = function ($event) {
+      $mdDialog.show({
+        controller: 'GetResultCtrl',
+        templateUrl: 'results_dialog.html',
+        locals: { result: $scope.exportAsCsv() },
+        parent: angular.element(document.body),
+        targetEvent: $event,
+        clickOutsideToClose:true,
+        fullscreen: true
+      });
+    };
+  }).
+  controller('GetResultCtrl', function ($scope, $mdDialog, result) {
+    $scope.result = result;
+
+    $scope.dismiss = function () {
+      $mdDialog.cancel();
+    };
+
+    $scope.emailCheck = function () {
+      return !$scope.email || !$scope.email.includes('@');
     };
   });
